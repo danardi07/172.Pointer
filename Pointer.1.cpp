5a0a53181f38cd7ac58cf79880349859c10544fc
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-05-08 15:27:11 +0700

    class mahasiswa

---
+++ Pointer.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim();
+};
+
+
+void mahasiswa::showNim() {
+	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa& ref = mhs;
+	ref.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	mhs.showNim();
+	return 0;
+
+
+}
